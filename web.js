83152ef03786bf8dcdc90e15307a11eb8294189e
dj <djshelton@gmail.com>
2013-07-10 21:23:15 +0000

    added web.js

--- web.js
+++ web.js
@@ -3,13 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var k = fs.readFileSync('index.html','utf8');
-var a = 'help me';
-//response.send(a);
-response.send('help meh hello worlsd');
- 
-//response.send(k);
-   //response.send(buf.toString(k));
+  var fs = require('fs');
+  var buf = new Buffer(fs.readFileSync('index.html'), 'utf-8');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
