0150bb6e28898500bce8126563fdaf6bd115bd79
dj <djshelton@gmail.com>
2013-07-10 20:46:19 +0000

    added web.js

--- web.js
+++ web.js
@@ -6,6 +6,11 @@ app.get('/', function(request, response) {
   response.send('Hello World 2!');
 });
 
+app.get('/', function(request, response) {
+    var k = fs.readFileSync('index.html','utf8');
+   response.send(buf.toString(k);
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
